a5997d0e43966eede2b22022350af7a657841c0f
Ewan Keith <ewan.keith100@gmail.com>
2026-01-04 20:53:04 +0000

    end of chapter 13 - Inheritance

--- jlox/src/main/java/com/craftinginterpeters/lox/Parser.java
+++ jlox/src/main/java/com/craftinginterpeters/lox/Parser.java
@@ -162,6 +162,13 @@ class Parser {
 
     private Stmt classDeclaration() {
         Token name = consume(IDENTIFIER, "Expect class name.");
+
+        Expr.Variable superclass = null;
+        if (match(LESS)) {
+            consume(IDENTIFIER, "Expect superclass name.");
+            superclass = new Expr.Variable(previous());
+        }
+
         consume(LEFT_BRACE, "Expect '{' before class body.");
 
         List<Stmt.Function> methods = new ArrayList<>();
@@ -171,7 +178,7 @@ class Parser {
 
         consume(RIGHT_BRACE, "Expect '}' after class body.");
 
-        return new Stmt.Class(name, methods);
+        return new Stmt.Class(name, superclass, methods);
     }
 
     private Stmt printStatement() {
@@ -347,6 +354,13 @@ class Parser {
 
         if (match(NUMBER, STRING)) {
             return new Expr.Literal(previous().literal);
+        }
+
+        if (match(SUPER)) {
+            Token keyword = previous();
+            consume(DOT, "Expect '.' after 'super;");
+            Token method = consume(IDENTIFIER, "Expect superlass method name.");
+            return new Expr.Super(keyword, method);
         }
 
         if (match(THIS)) {
