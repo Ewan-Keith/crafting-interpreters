a6de6af7fe9257935c60381d15c7ba2b0743921d
Ewan Keith <ewan.keith100@gmail.com>
2026-02-02 09:13:22 +0000

    end of chapter 19 - Strings

--- clox/src/vm.c
+++ clox/src/vm.c
@@ -3,10 +3,13 @@
 #include "common.h"
 #include "compiler.h"
 #include "debug.h"
+#include "memory.h"
+#include "object.h"
 #include "value.h"
 #include "vm.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 VM vm;
 
@@ -25,8 +28,11 @@ static void runtimeError(const char *format, ...) {
   resetStack();
 }
 
-void initVm() { resetStack(); }
-void freeVm() {}
+void initVm() {
+  resetStack();
+  vm.objects = NULL;
+}
+void freeVm() { freeObjects(); }
 
 void push(Value value) {
   *vm.stackTop = value;
@@ -42,6 +48,19 @@ static Value peek(int distance) { return vm.stackTop[-1 - distance]; }
 
 static bool isFalsey(Value value) {
   return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static void concatenate() {
+  ObjString *b = AS_STRING(pop());
+  ObjString *a = AS_STRING(pop());
+  int length = a->length + b->length;
+  char *chars = ALLOCATE(char, length + 1);
+  memcpy(chars, a->chars, a->length);
+  memcpy(chars + a->length, b->chars, b->length);
+  chars[length] = '\0';
+
+  ObjString *result = takeString(chars, length);
+  push(OBJ_VAL(result));
 }
 
 static InterpretResult run() {
@@ -103,7 +122,16 @@ static InterpretResult run() {
       break;
     }
     case OP_ADD: {
-      BINARY_OP(NUMBER_VAL, +);
+      if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+        concatenate();
+      } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+        double b = AS_NUMBER(pop());
+        double a = AS_NUMBER(pop());
+        push(NUMBER_VAL(a + b));
+      } else {
+        runtimeError("Operands must be two numvers or two strings.");
+        return INTERPRET_RUNTIME_ERROR;
+      }
       break;
     }
     case OP_SUBTRACT: {
